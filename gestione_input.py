185bd05fdf890306c572a01619c562b7297d7268
Gabriele Laguna <g.laguna@itsrizzoli.it>
2023-07-20 10:01:37 +0200

    Definite le funzioni

--- gestione_input.py
+++ gestione_input.py
@@ -6,25 +6,30 @@ PASSWORD = "oAcFTwdlvOYd4LOHfzSAx6_jv8umS-S_1E5g1HbYKn4"
 DATABASE_URL = "neo4j+s://3d212299.databases.neo4j.io"
 
 # Le funzioni di gestione degli input devono ritornare queste strinhe di esempio:
-QUERY_FIND_SUSPECT = f"""
-MATCH (p:Persona {{nome: {nome}}})
-MATCH (p)-[:Collegata]->(cella:Cella)
-RETURN cella.id AS cella_id
-"""
+def find_suspect_by_name(nome):
+    QUERY_FIND_SUSPECT = f"""
+    MATCH (p:Persona {{nome: {nome}}})
+    MATCH (p)-[:Collegata]->(cella:Cella)
+    RETURN cella.id AS cella_id
+    """
+    return QUERY_FIND_SUSPECT
 
-QUERY_FIND_SUSPECT_BY_LOCATION = f"""
-MATCH (p:Persona)-[:Collegata]->(cella:Cella)
-WHERE distance(point({{"latitude: "{latitudine}", longitude: "{longitudine}}}), point({{latitude: cella.latitudine, longitude: cella.longitudine}})) <= {raggio}
-AND {inizio} <= cella.Inizio_collegamento <= {fine}
-RETURN DISTINCT p.nome AS nome, p.cognome AS cognome, p.Sim AS sim
-"""
+def find_suspect_by_location(latitudine, longitudine, raggio, inizio, fine):
+    QUERY_FIND_SUSPECT_BY_LOCATION = f"""
+    MATCH (p:Persona)-[:Collegata]->(cella:Cella)
+    WHERE distance(point({{"latitude: "{latitudine}", longitude: "{longitudine}}}), point({{latitude: cella.latitudine, longitude: cella.longitudine}})) <= {raggio}
+    AND {inizio} <= cella.Inizio_collegamento <= {fine}
+    RETURN DISTINCT p.nome AS nome, p.cognome AS cognome, p.Sim AS sim
+    """
+    return QUERY_FIND_SUSPECT_BY_LOCATION
 
-
-QUERY_FIND_SIM_BY_CELL_AND_TIME = f"""
-MATCH (p:Persona)-[:Collegata]->(cella:Cella {{id: {cella_id}}})
-WHERE {inizio} <= cella.Inizio_collegamento <= {fine}
-RETURN DISTINCT p.nome AS nome, p.cognome AS cognome, p.Sim AS sim
-"""
+def find_suspect_by_cell(cella_id, inizio, fine):
+    QUERY_FIND_SIM_BY_CELL_AND_TIME = f"""
+    MATCH (p:Persona)-[:Collegata]->(cella:Cella {{id: {cella_id}}})
+    WHERE {inizio} <= cella.Inizio_collegamento <= {fine}
+    RETURN DISTINCT p.nome AS nome, p.cognome AS cognome, p.Sim AS sim
+    """
+    return QUERY_FIND_SIM_BY_CELL_AND_TIME
 
 # Test nuove funzioni
 if __name__ == "__main__":
