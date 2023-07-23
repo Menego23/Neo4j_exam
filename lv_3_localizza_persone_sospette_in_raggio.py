1a359acba5adc9ff1ac2b532fa7f71c22bc07e04
Gianluca Meneghetti <g.meneghetti2003@gmail.com>
2023-07-23 19:38:15 +0200

    funzione lv3 funzionante
    Co-Authored-By: TuffoPazzo <123157533+TuffoPazzo@users.noreply.github.com>

--- lv_3_localizza_persone_sospette_in_raggio.py
+++ lv_3_localizza_persone_sospette_in_raggio.py
@@ -1,26 +1,34 @@
 # file lv_3_localizza_persone_sospette_in_raggio.py
 
-def trova_persone_intestatarie_cella_in_raggio(driver, latitude, longitude, raggio, data, orario):
+def trova_persone_intestatarie_cella_in_raggio(driver):
+    latitudine = float(input("Inserisci la latitudine delle coordinate geografiche: "))
+    longitudine = float(input("Inserisci la longitudine delle coordinate geografiche: "))
+    data = input("Inserisci la data (YYYY-MM-DD): ")
+    orario = input("Inserisci l'orario (HH:MM:SS): ")
+
+    query = f"""
+        MATCH (persona:Persona)-[:Possiede]->(sim:Sim)-[collegata:Collegata]->(cella:Cella)-[:Situata]->(luogo:Luogo)
+        WHERE datetime('{data}T{orario}') >= collegata.data_inizio_collegamento AND
+              datetime('{data}T{orario}') <= collegata.data_fine_collegamento AND
+              6371000 * 2 * ASIN(SQRT(
+                  SIN(RADIANS(luogo.latitudine - {latitudine}) / 2) * SIN(RADIANS(luogo.latitudine - {latitudine}) / 2) +
+                  COS(RADIANS({latitudine})) * COS(RADIANS(luogo.latitudine)) *
+                  SIN(RADIANS(luogo.longitudine - {longitudine}) / 2) * SIN(RADIANS(luogo.longitudine - {longitudine}) / 2)
+              )) <= 1000
+        RETURN DISTINCT persona.nome AS nome_persona, sim.numero AS numero_sim
+    """
+
     with driver.session() as session:
-        result = session.run(
-            f"""
-            MATCH (p:Persona)-[:Possiede]->(s:Sim)-[collegata:Collegata]->(c:Cella)-[:Situata]->(luogo:Luogo)
-            WHERE point({{latitude: {latitude}, longitude: {longitude}}}).distance(point({{latitude: luogo.latitudine, longitude: luogo.longitudine}})) <= {raggio} * 1000 AND
-                  datetime('{data}T{orario}') >= collegata.data_inizio_collegamento AND
-                  datetime('{data}T{orario}') <= collegata.data_fine_collegamento
-            RETURN DISTINCT p.nome
-            """
-        )
-        
-        # Lista per salvare i nomi delle persone intestatarie delle SIM
-        persone_intestatarie = []
-        
-        # Itera sui record restituiti dalla query
+        result = session.run(query)
+
+        persone_in_raggio = []
+
         for record in result:
-            # Accedi al valore "nome" dal record
-            nome_persona = record["p.nome"]
-            # Aggiungi il nome della persona alla lista delle persone intestatarie
-            persone_intestatarie.append(nome_persona)
-        
-        # Stampa i nomi delle persone intestatarie delle SIM
-        print(f"Persone intestatarie delle SIM collegate alla cella in raggio di {raggio} km dalle coordinate ({latitude}, {longitude}) al {data} alle {orario}: {persone_intestatarie}")
+            nome_persona = record["nome_persona"]
+            numero_sim = record["numero_sim"]
+            persone_in_raggio.append((nome_persona, numero_sim))
+
+        print(f"Persone nel raggio di 1000 metri dalle coordinate ({latitudine}, {longitudine}) "
+              f"il {data} alle {orario}:")
+        for persona, numero_sim in persone_in_raggio:
+            print(f"Persona: {persona}, Numero SIM: {numero_sim}")
